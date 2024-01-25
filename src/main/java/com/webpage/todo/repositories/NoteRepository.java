9d76faa601a5187441ec161b64aad2a5328b0af9
MOHAMEDSAMEER78 <mohamedsameermms@gmail.com>
2024-01-25 12:05:44 +0530

    added todo changes

--- src/main/java/com/webpage/todo/repositories/NoteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.webpage.todo.repositories;
-import com.webpage.todo.model.notes;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NoteRepository extends MongoRepository<notes, String>{
-
-}
--- src/test/java/com/example/Todo/TodoApplicationTests.java
+++ src/test/java/com/example/Todo/TodoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.webpage.todo;
+package com.example.Todo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
